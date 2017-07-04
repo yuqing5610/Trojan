662d1348ab90e6072186c557329f14d93ff02d3f
yuqing5610 <616641808@qq.com>
2017-07-05 00:59:26 +0800

    Update dirlist.py

--- modules/dirlist.py
+++ modules/dirlist.py
@@ -7,7 +7,7 @@ def run(**args):
     dic = collections.defaultdict()
     info = ""
     print "[*] In dirlist module."
-    path = 'D:/test/'
+    path = 'D:/test/111.txt'
     name = str(path).replace('/', '_').replace(':', '')
     if not os.path.exists(path):
         dic[name] = "not exist\n"
